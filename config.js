012ecdf0e5c7eef526fffe8ad619071108568916
Tri D Bui <51746347+tridinhbui@users.noreply.github.com>
2022-01-27 23:48:32 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+
+   
+const CONFIG = {
+    introTitle: 'Xin chào NK!',
+    introDesc: `Cậu trả lời câu hỏi này cho tớ nhé :>>`,
+    btnIntro: 'Okie',
+    title: 'Hey I do have a crush on you, but do you have a crush on me^^',
+    desc: 'Chắc chắn là thích rồi nhưng tớ hỏi lại cho chắc thoi =)) ',
+    btnYes: 'Vẫn cứ là thích cậu',
+    btnNo: 'Không không khônggggg',
+    question:'Trên thế giới hơn 7 tỉ người mà cậu lại thích tớ luôn =))',
+    btnReply: 'Gửi cho cậu nèe',
+    reply: 'có tớ thích Bùi Đình Trí lam <333333333',
+    mess: 'Biết mà hehe',
+    messDesc: 'Hè này tớ về đứng dưới cửa nhà đợi đón đi chơii nhé',
+    btnAccept: 'Okelunn đi đâu thế :v',
+    messLink: 'https://goo.gl/maps/VzNo1aQu46ChhnDG8' 
+}
